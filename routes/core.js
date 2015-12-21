627c7ffc05d0457c14788e7f58abf1f6eab94d83
Keith Wade <keawade@gmail.com>
2015-12-21 15:31:46 -0600

    Network landing page implemented

--- routes/core.js
+++ routes/core.js
@@ -4,3 +4,15 @@ var router = module.exports = express.Router()
 router.get('/', function (req, res) {
   return res.render('index')
 })
+
+router.get('/network', function (req, res) {
+  return res.render('network')
+})
+
+router.get('/phone', function (req, res) {
+  return res.render('phone')
+})
+
+router.get('/misc', function (req, res) {
+  return res.render('misc')
+})
