0c3f5e086b56ce743786a9babc339beb05984c70
Keith Wade <keawade@gmail.com>
2016-01-27 11:51:46 -0600

    Restructured routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,36 @@
 var express = require('express')
 var router = module.exports = express.Router()
 
+var AlphaDB = require('./../models/alpha.js')
+
 // router.use(require('./auth'))
-router.use(require('./core'))
+
+router.get('/', function (req, res) {
+  return res.render('index')
+})
+
+router.use(require('./network'))
+
+router.get('/phone', function (req, res) {
+  return res.render('phone')
+})
+
+// Miscellaneous
+router.get('/misc', function (req, res, next) {
+  AlphaDB.find().sort('letter').exec(function (err, alphas) {
+    if (err) {
+      return next(err)
+    }
+    res.render('misc', {
+      alpha: alphas
+    })
+  })
+})
+
+router.get('/about', function (req, res, next) {
+  res.render('about', {
+    title: 'About'
+  })
+})
+
 // router.use(require('./error'))
